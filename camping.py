26d8a6cdfece0a555eac3e789de4f8b2cb2cfead
Daniel Porteous <danielporteous1@gmail.com>
2019-02-24 14:49:54 -0800

    Unify requests flow, make output for informative upon failure, create fake user agent

--- camping.py
+++ camping.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 
 import requests
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 BASE_URL = "https://www.recreation.gov"
 AVAILABILITY_ENDPOINT = "/api/camps/availability/campground/"
@@ -17,21 +18,21 @@ INPUT_DATE_FORMAT = "%Y-%m-%d"
 SUCCESS_EMOJI = "🏕"
 FAILURE_EMOJI = "❌"
 
+headers = {"User-Agent": UserAgent().random}
+
 
 def format_date(date_object):
     date_formatted = datetime.strftime(date_object, "%Y-%m-%dT00:00:00Z")
     return date_formatted
 
 
-def generate_payload(start, end):
-    payload = {"start_date": format_date(start), "end_date": format_date(end)}
-    return payload
+def generate_params(start, end):
+    params = {"start_date": format_date(start), "end_date": format_date(end)}
+    return params
 
 
-def send_request(park_id, payload):
-    url = "{}{}{}".format(BASE_URL, AVAILABILITY_ENDPOINT, park_id)
-    resp = requests.get(url, params=payload)
-
+def send_request(url, params):
+    resp = requests.get(url, params=params, headers=headers)
     if resp.status_code != 200:
         raise RuntimeError(
             "failedRequest",
@@ -42,10 +43,15 @@ def send_request(park_id, payload):
     return resp.json()
 
 
+def get_park_information(park_id, params):
+    url = "{}{}{}".format(BASE_URL, AVAILABILITY_ENDPOINT, park_id)
+    return send_request(url, params)
+
+
 def get_name_of_site(park_id):
     url = "{}{}{}".format(BASE_URL, MAIN_PAGE_ENDPOINT, park_id)
-    resp = requests.get(url)
-    return resp.json()["campground"]["facility_name"]
+    resp = send_request(url, {})
+    return resp["campground"]["facility_name"]
 
 
 def get_num_available_sites(resp, start_date, end_date):
@@ -79,10 +85,7 @@ def valid_date(s):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--start-date",
-        required=True,
-        help="Start date [YYYY-MM-DD]",
-        type=valid_date,
+        "--start-date", required=True, help="Start date [YYYY-MM-DD]", type=valid_date
     )
     parser.add_argument(
         "--end-date",
@@ -104,22 +107,34 @@ if __name__ == "__main__":
 
     parks = args.parks or [p.strip() for p in sys.stdin]
 
-    print(
-        "There are campsites available from {} to {}!!!".format(
-            args.start_date.strftime(INPUT_DATE_FORMAT),
-            args.end_date.strftime(INPUT_DATE_FORMAT),
-        )
-    )
+    out = []
+    availabilities = False
     for park_id in parks:
-        payload = generate_payload(args.start_date, args.end_date)
-        resp_json = send_request(park_id, payload)
+        params = generate_params(args.start_date, args.end_date)
+        park_information = get_park_information(park_id, params)
         name_of_site = get_name_of_site(park_id)
         current, maximum = get_num_available_sites(
-            resp_json, args.start_date, args.end_date
+            park_information, args.start_date, args.end_date
         )
-        emoji = SUCCESS_EMOJI if current else FAILURE_EMOJI
-        print(
+        if current:
+            emoji = SUCCESS_EMOJI
+            availabilities = True
+        else:
+            emoji = FAILURE_EMOJI
+
+        out.append(
             "{} {}: {} site(s) available out of {} site(s)".format(
                 emoji, name_of_site, current, maximum
             )
         )
+
+    if availabilities:
+        print(
+            "There are campsites available from {} to {}!!!".format(
+                args.start_date.strftime(INPUT_DATE_FORMAT),
+                args.end_date.strftime(INPUT_DATE_FORMAT),
+            )
+        )
+    else:
+        print("There are no campsites available :(")
+    print("\n".join(out))
