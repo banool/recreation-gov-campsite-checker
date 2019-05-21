b77400c6cb6df5a96d3d50c713f1fcdbd0f43985
Daniel Porteous <dport@fb.com>
2019-05-20 18:43:18 -0700

    Remove twitter credentials, can't trust anyone :(

--- twitter_credentials.py
+++
@@ -1,6 +0,0 @@
-twitter_credentials = {
-    "consumer_key": "qLALF0eACjYMlEA1ar6fhiS7G",
-    "consumer_secret": "KjLSb4t2G5IKk4OsRzBK1nfoYaOHCXn1zacxpS4Gwut9z8HRli",
-    "access_token_key": "1018202969378992129-jeWnC9JKtfdZHhY1bnLAwQgIRpjoAS",
-    "access_token_secret": "e84EGoME2JDhsuihG4XKLbIyJT0qLiWOG1WbkhprfT25b",
-}
