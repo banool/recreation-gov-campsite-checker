4d9d1c560b6928c835d691d0a7e375980f3dd24a
Chris Streeter <chris@chrisstreeter.com>
2019-11-06 20:37:23 -0800

    Clean up the script

--- cron.sh
+++ cron.sh
@@ -11,9 +11,14 @@ PATH="/usr/local/bin:${PATH}"
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 TERMINAL_NOTIFIER=`which terminal-notifier`
-NOTIF_ARGS="-appIcon $BASE_DIR/camping.png"
 
-echo "${@:1}"
+if [ ! -e $TERMINAL_NOTIFIER ]; then
+    [ "${INTERACTIVE}" -eq "1" ] && echo "terminal-notifier does not exist on the path"
+    exit 1
+fi
+
+
+NOTIF_ARGS="-appIcon $BASE_DIR/camping.png"
 
 if [ -z "$VIRTUAL_ENV" ]; then
     [ "${INTERACTIVE}" -eq "1" ] && echo "Not running in a virtualenv, this is not recommended! Set up your virtualenv use it by setting VIRTUAL_ENV"
@@ -28,10 +33,8 @@ if [ -z "$result" ] ; then
     # No sites available
 else
     # We've got a site available
-    if [ -e $TERMINAL_NOTIFIER ]; then
-        # Send to the Notification Center
-        $TERMINAL_NOTIFIER $NOTIF_ARGS \
-            -title "Campsites Are Available" \
-            -message "$result"
-    fi
+    # Send to the Notification Center
+    $TERMINAL_NOTIFIER $NOTIF_ARGS \
+        -title "Campsites Are Available" \
+        -message "$result"
 fi
