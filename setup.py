8c1518b22973714c6aab9029c262cd10532b7020
Daniel Porteous <danielporteous1@gmail.com>
2021-03-16 17:04:39 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+README_FILE = "README.md"
+REQUIREMENTS_FILE = "requirements.txt"
+
+
+setup(
+    name="recreation_gov_campsite_checker",
+    packages=[""],
+    version=1.0,
+    description="Scrape recreation.gov for campsite availability",
+    long_description=open(README_FILE).read(),
+    url="https://github.com/banool/recreation-gov-campsite-checker",
+    author="Daniel Porteous and contributors",
+    install_requires=open(REQUIREMENTS_FILE).read().splitlines(),
+    include_package_data=True,
+)
